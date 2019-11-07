30104aa392748bdcf37e8b1410bfc2219fb019fc
user917343 <user917343@74-80-250-211.ic2net.net>
2019-11-07 11:54:50 -0800

    hello world ios app

---
+++ Stat Tacker/Stat Tacker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stat Tacker
+//
+//  Created by user917343 on 11/7/19.
+//  Copyright © 2019 user917343. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
